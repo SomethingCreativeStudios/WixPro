58b77ccb98257f435476727f3c190db6cd261415
Eric <eric-417@live.com>
2017-02-12 02:57:45 -0500

    Adding WSCardSearchService

---
+++ Wix Studio/Wix Studio/WixCardSearchService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wix_Studio
+{
+    public class WixCardSearchService
+    {
+        public static List<WixossCard> findCards(WixossCard searchCard)
+        {
+            List<WixossCard> resultCards = new List<WixossCard>();
+
+            return resultCards;
+        }
+    }
+}
