7b6824914156ab53fbd7cad136dd5229077ea046
SomethingCreativeStudios <eric-417@live.com>
2017-03-26 14:05:55 -0400

    Updateing deck builder

--- Wix Studio/Wix Studio/Deck Builder/DeckBuilderGUI.xaml.cs
+++ Wix Studio/Wix Studio/Deck Builder/DeckBuilderGUI.xaml.cs
@@ -138,11 +138,22 @@ namespace Wix_Studio.Deck_Builder
 
         private int handleListViewMouseMove(object sender , System.Windows.Input.MouseEventArgs e)
         {
-            int selectedIndex = this.GetCurrentIndex(e.GetPosition , (System.Windows.Controls.ListView)sender);
+            System.Windows.Controls.ListView listView = null;
+
+            if ( sender.GetType() == typeof(Grid) )
+            {
+                System.Windows.Controls.ListViewItem listViewItem = sender as System.Windows.Controls.ListViewItem;
+                listView = ItemsControl.ItemsControlFromItemContainer(listViewItem) as System.Windows.Controls.ListView;
+            } else
+            {
+                listView = sender as System.Windows.Controls.ListView;
+            }
+
+            int selectedIndex = this.GetCurrentIndex(e.GetPosition , listView);
             if ( selectedIndex == -1 )
                 return -1;
 
-            selectCard(selectedIndex , (System.Windows.Controls.ListView)sender);
+            selectCard(selectedIndex , listView);
 
             return selectedIndex;
         }
--- Wix Studio/Wix Studio/WixCardFiles/WixCardSearchService.cs
+++ Wix Studio/Wix Studio/WixCardFiles/WixCardSearchService.cs
@@ -14,6 +14,7 @@ namespace Wix_Studio
             CardCollection cardCollection = new CardCollection();
             List<WixossCard> resultCards = new List<WixossCard>();
             List<WixossCard> totalCards = ( searchCard.setName != "" ? cardCollection.GetCardsInSets(searchCard.setName) : CardCollection.cardCollection.Values.ToList() );
+
             foreach ( var wixCard in totalCards)
             {
                 Boolean addCard = true;// searchCard.isEmpty();
@@ -102,5 +103,6 @@ namespace Wix_Studio
 
             return inRange;
         }
+        
     }
 }
